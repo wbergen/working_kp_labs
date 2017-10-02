2f6f249aa25e79fa2a9ea3e35dfcb9bb6c5f5254
emanuele vineti <emanuele.vineti@gmail.com>
2017-10-02 14:01:54 +0200

    fixes

--- lib/fork.c
+++ lib/fork.c
@@ -7,7 +7,9 @@ envid_t fork(void)
 {
     /* LAB 5: Your code here. */
     envid_t i = sys_fork();
-    //if (i == )
-    //cprintf("env: %x forked -> %x\n", thisenv->env_id, i);
+    if (i == 0){
+    	thisenv = &envs[ENVX(sys_getenvid())];
+    }
+    cprintf("env: %x forked -> %x\n", thisenv->env_id, i);
     return i;
 }
